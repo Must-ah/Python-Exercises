e2bd6cadda346c7fb4711203685607963da5def7
Must-ah <Mustafa.Abdel-Hussein@hotmail.com>
2021-10-21 10:36:35 +0200

    Ex.1 Finished Focused on secure code by type checking

--- 1_Numerical_types/Exercise 1 Number guessing game.py
+++ 1_Numerical_types/Exercise 1 Number guessing game.py
@@ -1,8 +1,23 @@
 
-def guessing_game():
+def guessing_game()-> None:
     from random import randint
     my_guessing_value = randint(0,100)
-    return my_guessing_value
+    user_guessed = -10
+
+    while user_guessed != my_guessing_value:
+        user_guess_str = input(("Guess a number between 0 and 100 -> "))
+        assert user_guess_str.isdigit() == True
+        user_guessed = int(user_guess_str)
+        message_str = f"Your guess of {user_guessed} is too "
+        if user_guessed > my_guessing_value:
+            print(message_str+"high!")
+        else:
+            print(message_str+"low!")
+
+    
+    print(f"Congratulation you guessed correct!")
+
+guessing_game()
 
     
 
